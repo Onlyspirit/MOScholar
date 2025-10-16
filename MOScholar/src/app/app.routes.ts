941331338a5a33569d53199513cf3480ca618ee1
Onlyspirit <miquestdeji@gmail.com>
2025-10-16 13:29:38 +0100

    landing page, who knows

--- MOScholar/src/app/app.routes.ts
+++ MOScholar/src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { Home } from './home/home';
 import { Signup } from './signup/signup';
 import { Login } from './login/login';
 import { Pagenotfound } from './pagenotfound/pagenotfound';
+import { Landing } from './landingpage/landingpage';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' }, // ✅ default route
+  { path: 'landing', component: Landing },
+  { path: 'home', component: Home },
   { path: 'signup', component: Signup },
   { path: 'login', component: Login },
-  { path: 'home', component: Home },
   { path: '404page', component: Pagenotfound },
-  { path: 'home', component: Home },
-  { path: '**', redirectTo: '404page' },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-
+  { path: '**', redirectTo: '404page' } // ✅ wildcard last
 ];
